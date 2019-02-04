181c98cad9269661e4e17a1428970d7dd2929bed
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-04 17:17:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  
+  def initialize
+    @@count += 1 
+  end
+  
+  def count
+  end
+  
+  def genres 
+  end
+  
+end
